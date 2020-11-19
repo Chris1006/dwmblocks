3cab002b675b17b073142e53864a63e201831621
christian poß <christianposs@gmx.de>
2020-11-19 07:30:02 +0100

    changed status bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,23 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    {"", "~/.local/bin/statusbar/moonphase", 3600, 0},
+    {"", "~/.local/bin/statusbar/weather", 60, 0},
+    {"", "~/.local/bin/statusbar/cpu_temp", 15, 0},
+    {"", "~/.local/bin/statusbar/cpu_usage", 15, 0},
+    {"", "~/.local/bin/statusbar/memory", 60, 0},
+    {"", "~/.local/bin/statusbar/free_disk_space", 60, 0},
+    {"", "~/.local/bin/statusbar/ip_addr", 20, 0},
+    {"", "~/.local/bin/statusbar/battery", 5, 0},
+    {"", "~/.local/bin/statusbar/mailbox", 0, 19},
+    {"", "~/.local/bin/statusbar/news", 0, 14},
+    {"", "~/.local/bin/statusbar/pacman", 0, 13},
+    {"", "~/.local/bin/statusbar/volume-new", 0, 10},
+    {"", "~/.local/bin/statusbar/calendar", 60, 0},
+    {"", "~/.local/bin/statusbar/clock", 30, 0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
